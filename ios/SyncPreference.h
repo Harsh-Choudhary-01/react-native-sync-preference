4dc033313c8b9df1cc52421fae7478b48598754d
Harsh Choudhary <harshch000@gmail.com>
2020-01-20 16:28:01 -0600

    objc code

---
+++ ios/SyncPreference.h
@@ -0,0 +1,5 @@
+#import <React/RCTBridgeModule.h>
+
+@interface SyncPreference : NSObject <RCTBridgeModule>
+
+@end
--- ios/SyncPreference.m
+++ ios/SyncPreference.m
@@ -1,11 +1,70 @@
-#import <React/RCTBridgeModule.h>
 
-@interface RCT_EXTERN_MODULE(SyncPreference, NSObject)
+#import "SyncPreference.h"
 
-RCT_EXTERN_METHOD(clearMultiple:(NSArray*)keys resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
-RCT_EXTERN_METHOD(clearAll:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
-RCT_EXTERN_METHOD(clear:(NSString*)key resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
-RCT_EXTERN_METHOD(set:(NSString*)key value:(NSString*)value resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
-RCT_EXTERN_METHOD(setMultiple:(NSDictionary*)data resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+@implementation SyncPreference
+
+- (NSUserDefaults *) getDefaultUser{
+    return [NSUserDefaults standardUserDefaults];
+}
+
+- (NSDictionary *) getAll {
+    NSArray *keys = [[[self getDefaultUser] dictionaryRepresentation] allKeys];
+    NSMutableDictionary *result = [NSMutableDictionary dictionary];
+    for(NSString *key in keys) {
+        NSString *value = [[self getDefaultUser] stringForKey:key];
+        result[key] = value == nil ? [NSNull null] : value;
+    }
+    return result
+}
+
+RCT_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD(set:(NSString *)key value:(NSString *)value
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(__unused RCTPromiseRejectBlock)reject)
+{
+    [[self getDefaultUser] setObject:value forKey:key];
+    resolve([NSNull null]);
+}
+
+RCT_EXPORT_METHOD(clear:(NSString *)key
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(__unused RCTPromiseRejectBlock)reject)
+{
+    [[self getDefaultUser] removeObjectForKey:key];
+    resolve([NSNull null]);
+}
+
+RCT_EXPORT_METHOD(setMultiple:(NSDictionary *)data
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(__unused RCTPromiseRejectBlock)reject)
+{
+    for (id key in data) {
+        [[self getDefaultUser] setObject:[data objectForKey:key] forKey:key];
+    }
+    resolve([NSNull null]);
+}
+
+RCT_EXPORT_METHOD(clearMultiple:(NSArray *)keys
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(__unused RCTPromiseRejectBlock)reject)
+{
+    for(NSString *key in keys) {
+        [[self getDefaultUser] removeObjectForKey:key];
+    }
+    resolve([NSNull null]);
+}
+
+- (NSDictionary *)constantsToExport
+{
+  return @{ @"InitialPrefs": [self getAll] };
+}
+
+RCT_EXPORT_METHOD(clearAll:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject)
+{
+    NSArray *keys = [[[self getDefaultUser] dictionaryRepresentation] allKeys];
+    [self clearMultiple:keys resolve:resolve reject:reject];
+}
 
 @end
